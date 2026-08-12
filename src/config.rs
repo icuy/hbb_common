783be0bace682523dda0008bb92d1ad128aadfdf
rustdesk-build <build@local>
2026-08-12 15:14:46 +0800

    chore: default rendezvous server -> rd.sglyk.com + custom key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.sglyk.com"];
+pub const RS_PUB_KEY: &str = "71qit0jH4TJsbCxHOZQMvYUDugl0KML+i3Q3oJmRtX8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
